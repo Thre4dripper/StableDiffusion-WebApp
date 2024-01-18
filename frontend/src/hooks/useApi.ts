c0a58acc9294e0a22e29611065efccec700b4f36
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-01-18 19:28:32 +0530

    revised useApi hook

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -1,81 +1,74 @@
 import { useState, useCallback } from 'react'
-import axios, { AxiosError, AxiosProgressEvent, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+export enum RequestMethod {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+}
 
 interface UseApiConfig {
     url: string
-    method?: AxiosRequestConfig['method']
-    headers?: AxiosRequestConfig['headers']
-    body?: AxiosRequestConfig['data']
-    onDownloadProgress?: (progressEvent: AxiosProgressEvent) => void
-    onUploadProgress?: (progressEvent: AxiosProgressEvent) => void
-    params?: AxiosRequestConfig['params']
+    method?: RequestMethod
+}
+
+interface CallFunction {
+    body: any
+    token?: string
+    onSuccess: (data: AxiosResponse | null) => void
+    onError: (error: string) => void
 }
 
 interface UseApiResult {
-    data: AxiosResponse | null
-    error: string | null
+    callApi: (callFunction: CallFunction) => void
     isLoading: boolean
     isSuccess: boolean
     isFailed: boolean
-    callApi: () => void
-    reset: () => void
+    isIdle: boolean
 }
 
-const useApi = ({
-    url,
-    method = 'GET',
-    headers,
-    body,
-    onDownloadProgress,
-    onUploadProgress,
-    params,
-}: UseApiConfig): UseApiResult => {
-    const [data, setData] = useState<AxiosResponse | null>(null)
-    const [error, setError] = useState<string | null>(null)
+const useApi = ({ url, method = RequestMethod.GET }: UseApiConfig): UseApiResult => {
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [isSuccess, setIsSuccess] = useState<boolean>(false)
     const [isFailed, setIsFailed] = useState<boolean>(false)
 
-    const callApi = useCallback(() => {
-        setIsLoading(true)
+    const callApi = useCallback(
+        ({ body, token, onSuccess, onError }: CallFunction) => {
+            setIsLoading(true)
 
-        axios({
-            url,
-            method,
-            headers,
-            data: body,
-            onDownloadProgress,
-            onUploadProgress,
-            params,
-        })
-            .then((response: AxiosResponse) => {
-                setData(response)
-                setIsLoading(false)
-                setIsSuccess(true)
-            })
-            .catch((error: AxiosError) => {
-                setError(error.message)
-                setIsFailed(true)
-                setIsLoading(false)
-            })
-    }, [url, method, headers, body, onDownloadProgress, onUploadProgress, params])
+            const axiosConfig: AxiosRequestConfig = {
+                url,
+                method,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                data: body,
+            }
 
-    const reset = () => {
-        setData(null)
-        setError(null)
-        setIsLoading(false)
-        setIsSuccess(false)
-        setIsFailed(false)
-    }
+            if (token) axiosConfig.headers!['Authorization'] = `Bearer ${token}`
+
+            axios(axiosConfig)
+                .then((response: AxiosResponse) => {
+                    setIsLoading(false)
+                    setIsSuccess(true)
+                    onSuccess(response)
+                })
+                .catch((error: any) => {
+                    setIsFailed(true)
+                    setIsLoading(false)
+                    onError(error.error)
+                })
+        },
+        [url, method]
+    )
 
     return {
-        data,
-        error,
+        callApi,
         isLoading,
         isSuccess,
         isFailed,
-        callApi,
-        reset,
+        isIdle: !isLoading && !isSuccess && !isFailed,
     }
 }
 
