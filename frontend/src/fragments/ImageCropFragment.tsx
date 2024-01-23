46321a6c0a3b495a20e5316de40ed90880af9378
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-01-23 14:54:32 +0530

    implemented rotation in canvas preview

--- frontend/src/fragments/ImageCropFragment.tsx
+++ frontend/src/fragments/ImageCropFragment.tsx
@@ -4,7 +4,8 @@ import Button from '@mui/material/Button'
 import { centerCrop, convertToPixelCrop, Crop, makeAspectCrop, ReactCrop } from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
 import Typography from '@mui/material/Typography'
-import { setCanvasPreview } from '../utils/Utils.ts'
+import { CropRotate } from '@mui/icons-material'
+import CustomSamplingSlider from '../components/sliders/CustomSamplingSlider.tsx'
 
 interface ImageCropFragmentProps {
     cropShape: 'rect' | 'round'
@@ -17,12 +18,15 @@ const ImageCropFragment: React.FC<ImageCropFragmentProps> = ({ cropShape, aspect
     const imageRef = useRef<HTMLImageElement>(null)
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
+    const [rotation, setRotation] = React.useState(0)
+
     const onCropComplete = (crop: Crop) => {
         const { width, height } = imageRef.current!
         setCanvasPreview(
             imageRef.current!,
             canvasRef.current!,
-            convertToPixelCrop(crop, width, height)
+            convertToPixelCrop(crop, width, height),
+            rotation
         )
     }
 
@@ -39,6 +43,51 @@ const ImageCropFragment: React.FC<ImageCropFragmentProps> = ({ cropShape, aspect
         )
 
         setCrop(centerCrop(crop, width, height))
+    }
+
+    const setCanvasPreview = (
+        image: HTMLImageElement,
+        canvas: HTMLCanvasElement,
+        crop: Crop,
+        rotation: number
+    ) => {
+        const ctx = canvas.getContext('2d')
+        if (!ctx) {
+            return
+        }
+
+        const pixelRatio = window.devicePixelRatio
+        const scaleX = image.naturalWidth / image.width
+        const scaleY = image.naturalHeight / image.height
+
+        canvas.width = crop.width * pixelRatio * scaleX
+        canvas.height = crop.height * pixelRatio * scaleY
+
+        ctx.scale(pixelRatio, pixelRatio)
+        ctx.imageSmoothingQuality = 'high'
+        const cropX = crop.x * scaleX
+        const cropY = crop.y * scaleY
+
+        ctx.translate(canvas.width / 2, canvas.height / 2)
+        ctx.rotate((rotation * Math.PI) / 180)
+        ctx.translate(-canvas.width / 2, -canvas.height / 2)
+
+        // ctx.translate((-crop.width * scaleX) / 2, (-crop.height * scaleY) / 2)
+        ctx.translate(-cropX, -cropY)
+
+        ctx.drawImage(
+            image,
+            0,
+            0,
+            image.naturalWidth,
+            image.naturalHeight,
+            0,
+            0,
+            image.naturalWidth,
+            image.naturalHeight
+        )
+
+        ctx.restore()
     }
 
     return (
@@ -83,9 +132,34 @@ const ImageCropFragment: React.FC<ImageCropFragmentProps> = ({ cropShape, aspect
                     aspect={aspect}>
                     <img src={image ?? ''} alt={''} ref={imageRef} onLoad={onImageLoaded} />
                 </ReactCrop>
-
+                {/*Controls*/}
+                <div className={'flex flex-row gap-8 justify-center items-center mt-8'}>
+                    <CropRotate />
+                    <CustomSamplingSlider
+                        value={rotation}
+                        min={0}
+                        max={360}
+                        valueLabelDisplay='auto'
+                        color='warning'
+                        step={1}
+                        onChange={(_event: Event, newValue: number | number[]) => {
+                            const value = newValue as number
+                            setRotation(value)
+                            setCanvasPreview(
+                                imageRef.current!,
+                                canvasRef.current!,
+                                convertToPixelCrop(
+                                    crop!,
+                                    imageRef.current!.width,
+                                    imageRef.current!.height
+                                ),
+                                value
+                            )
+                        }}
+                    />
+                </div>
                 <div className={'flex flex-row gap-4 justify-end items-center'}>
-                    <Button variant='outlined' color='error'>
+                    <Button variant='outlined' color='warning'>
                         Cancel
                     </Button>
                     <Button variant='contained' color='warning'>
