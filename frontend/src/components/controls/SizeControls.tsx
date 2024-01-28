a54c3f70142abf2a4605cd9fad935d56111e19c9
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-01-28 19:50:46 +0530

    displaying size labels based on a selected model

--- frontend/src/components/controls/SizeControls.tsx
+++ frontend/src/components/controls/SizeControls.tsx
@@ -5,36 +5,65 @@ import { useDispatch, useSelector } from 'react-redux'
 import { DimensionsInitialState } from '../../redux/reducers/dimensionsReducer.ts'
 import { setHeight, setWidth } from '../../redux/actions/dimensionsActions.ts'
 import { RootState } from '../../redux/store.ts'
+import { Model } from '../../redux/reducers/authReducer.ts'
 
-const marks = [
-    {
-        value: 0,
-        label: '32',
-    },
-    {
-        value: 20,
-        label: '64',
-    },
-    {
-        value: 40,
-        label: '128',
-    },
-    {
-        value: 60,
-        label: '256',
-    },
-    {
-        value: 80,
-        label: '512',
-    },
-    {
-        value: 100,
-        label: '1024',
-    },
-]
+const marks = {
+    [Model.WIZ_MODEL]: [
+        {
+            value: 0,
+            label: '32',
+        },
+        {
+            value: 20,
+            label: '64',
+        },
+        {
+            value: 40,
+            label: '128',
+        },
+        {
+            value: 60,
+            label: '256',
+        },
+        {
+            value: 80,
+            label: '512',
+        },
+        {
+            value: 100,
+            label: '1024',
+        },
+    ],
+    [Model.STABILITY_AI]: [
+        {
+            value: 0,
+            label: '320',
+        },
+        {
+            value: 20,
+            label: '448',
+        },
+        {
+            value: 40,
+            label: '640',
+        },
+        {
+            value: 60,
+            label: '896',
+        },
+        {
+            value: 80,
+            label: '1280',
+        },
+        {
+            value: 100,
+            label: '1536',
+        },
+    ],
+}
 
-const labelFormat = (value: number) => {
-    return marks.find((mark) => mark.value === value)?.label || undefined
+const labelFormat = (value: number, model: Model) => {
+    return marks[model].find((mark) => mark.value === value)?.label || undefined
 }
 
 interface SizeControlsProps {
@@ -46,17 +75,23 @@ const SizeControls: React.FC<SizeControlsProps> = ({ index }) => {
         (state) => state.dimensions[index]
     )
 
-    const widthValue = marks.find((mark) => +mark.label === width)?.value || 0
-    const heightValue = marks.find((mark) => +mark.label === height)?.value || 0
+    const selectedModel = useSelector<RootState, Model>(
+        (state) => state.auth.userData?.model ?? Model.WIZ_MODEL
+    )
+
+    const widthValue = marks[selectedModel].find((mark) => +mark.label === width)?.value || 0
+    const heightValue = marks[selectedModel].find((mark) => +mark.label === height)?.value || 0
 
     const dispatch = useDispatch()
     const handleWidthChange = (_event: Event, newValue: number | number[]) => {
-        const value = marks.find((mark) => mark.value === (newValue as number))?.label || '0'
+        const value =
+            marks[selectedModel].find((mark) => mark.value === (newValue as number))?.label || '0'
         dispatch(setWidth(+value, index))
     }
 
     const handleHeightChange = (_event: Event, newValue: number | number[]) => {
-        const value = marks.find((mark) => mark.value === (newValue as number))?.label || '0'
+        const value =
+            marks[selectedModel].find((mark) => mark.value === (newValue as number))?.label || '0'
         dispatch(setHeight(+value, index))
     }
 
@@ -73,9 +108,9 @@ const SizeControls: React.FC<SizeControlsProps> = ({ index }) => {
                     <label className={'text-black text-md'}>Width</label>
                     <CustomMarksSlider
                         valueLabelDisplay='auto'
-                        valueLabelFormat={labelFormat}
+                        valueLabelFormat={(value) => labelFormat(value, selectedModel)}
                         aria-label='pretto slider'
-                        marks={marks}
+                        marks={marks[selectedModel]}
                         value={widthValue}
                         onChange={handleWidthChange}
                         step={null}
@@ -87,10 +122,10 @@ const SizeControls: React.FC<SizeControlsProps> = ({ index }) => {
                         valueLabelDisplay='auto'
                         aria-label='pretto slider'
                         value={heightValue}
-                        valueLabelFormat={labelFormat}
+                        valueLabelFormat={(value) => labelFormat(value, selectedModel)}
                         step={null}
                         onChange={handleHeightChange}
-                        marks={marks}
+                        marks={marks[selectedModel]}
                     />
                 </div>
             </div>
