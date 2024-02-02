bb690d79d200a5601ed928306977a6fae11be5c8
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-02-02 23:57:28 +0530

    removed auth requirements from link preview

--- backend/src/app/routes/link.preview.router.ts
+++ backend/src/app/routes/link.preview.router.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import LinkPreviewController from '../apis/link_preview/controllers/link.preview.controller'
-import Auth from '../middlewares/Auth'
 
 export default (app: express.Application) => {
-    LinkPreviewController.post(app, '/api/v1/link-preview', [Auth])
+    LinkPreviewController.post(app, '/api/v1/link-preview', [])
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        proxy: {
-            '/local': {
-                target: 'http://127.0.0.1:7860',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/local/, ''),
-            },
-            '/github': {
-                target: 'https://github.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/github/, ''),
-            },
-        },
-    },
     plugins: [react()],
 })
